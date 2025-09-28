c2d34979c70a4da480926885cdd83b1f08b89d2a
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2025-09-28 22:24:26 +0200

    image

--- grymus/category-aktualnosci.php
+++ grymus/category-aktualnosci.php
@@ -13,14 +13,14 @@ get_header();
 <header class="grid content-center bg-secondary p-4">
     <h1 class="headPage text-on-secondary"><?php single_cat_title(); ?></h1>
 </header>
-<main class="grid justify-center">
-    <div class="grid gap-2 py-2">
+<main>
+    <div class="grid gap-2 py-2 max-w-[120ch] mx-auto px-1 @md:px-4">
         <?php $delay = 100; ?>
         <?php
         while (have_posts()) :
             the_post();
         ?>
-            <div class="grid grid-cols-1 p-1 @md:grid-cols-2 @md:p-2 shadow-2xl items-center justify-center bg-background/80 rounded w-full slideInUp load-on-view">
+            <div class="grid grid-rows-[1fr_2fr] @md:grid-rows-1 grid-cols-1 p-1 @md:grid-cols-[1fr_2fr] @md:p-2 shadow-2xl items-center justify-center bg-background/80 rounded w-full h-full slideInUp load-on-view">
                 <?php
                 if (has_post_thumbnail()) {
                     $image = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), 'thumbnail');
@@ -29,13 +29,12 @@ get_header();
                     $image = esc_url(get_template_directory_uri() . '/images/grymus_logo_250.webp');
                 }
                 ?>
-                <div class="p-1">
-                    <img src="<?php echo $image ?>" alt="">
+                <div class="p-1 bg-contain bg-center bg-no-repeat w-full h-full rounded" style="background-image: url('<?php echo $image ?>');">
                 </div>
-                <div class="grid grid-rows-[auto_auto_auto] p-1 border prose prose-slate border-green-600">
-                    <h2><?php the_title() ?></h2>
+                <div class="grid grid-rows-[auto_auto_auto] p-1 prose prose-slate">
+                    <h2 class="text-center"><?php the_title() ?></h2>
                     <?php the_excerpt(); ?>
-                    <div class="flex justify-end w-full p-1 border border-red-600">
+                    <div class="flex justify-end w-full">
                         <a class="btn" href="<?php the_permalink(); ?>">
                             <span>pokaż więcej</span>
                         </a>
